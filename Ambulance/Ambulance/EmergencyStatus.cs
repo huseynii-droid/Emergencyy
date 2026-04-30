efd88af532b0fd8df4d37dfa74dc15fa17eacc1e
huseynii-droid <huseynii@code.edu.az>
2026-04-30 18:12:46 +0400

    featureandmodels

---
+++ Ambulance/Ambulance/EmergencyStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ambulance
+{
+    internal enum EmergencyStatus
+    {
+        Created,
+        Assigned,
+        OnRoute,
+        Completed,
+    }
+}
